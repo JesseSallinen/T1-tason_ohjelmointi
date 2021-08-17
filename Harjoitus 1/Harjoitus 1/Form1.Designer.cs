b1398f1b1ea8bdba2888438c4a1e5a514deb5677
JesseSallinen <89012854+JesseSallinen@users.noreply.github.com>
2021-08-17 14:14:22 +0300

    Add files via upload
    Harjoitus ykkönen

---
+++ Harjoitus 1/Harjoitus 1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Harjoitus_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.VaihdaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OtsikkoLB.Location = new System.Drawing.Point(68, 41);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(94, 32);
+            this.OtsikkoLB.TabIndex = 0;
+            this.OtsikkoLB.Text = "Otsikko";
+            // 
+            // VaihdaBT
+            // 
+            this.VaihdaBT.Location = new System.Drawing.Point(68, 102);
+            this.VaihdaBT.Name = "VaihdaBT";
+            this.VaihdaBT.Size = new System.Drawing.Size(94, 45);
+            this.VaihdaBT.TabIndex = 1;
+            this.VaihdaBT.Text = "Vaihda teksti";
+            this.VaihdaBT.UseVisualStyleBackColor = true;
+            this.VaihdaBT.Click += new System.EventHandler(this.VaihdaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 658);
+            this.Controls.Add(this.VaihdaBT);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Name = "Form1";
+            this.Text = "Harjoitus 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OtsikkoLB;
+        private System.Windows.Forms.Button VaihdaBT;
+    }
+}
+
