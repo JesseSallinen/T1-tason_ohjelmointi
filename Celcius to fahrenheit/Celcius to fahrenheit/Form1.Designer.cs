3b5c43fc2b8c4ad1721f1107ea195d482e7b952b
Jesse Sallinen <89012854+JesseSallinen@users.noreply.github.com>
2021-08-24 12:56:05 +0300

    Add files via upload
    Celsius to fahrenheit tehtävä

---
+++ Celcius to fahrenheit/Celcius to fahrenheit/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Celcius_to_fahrenheit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CelsiusRB = new System.Windows.Forms.RadioButton();
+            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MuunnaBT = new System.Windows.Forms.Button();
+            this.AsteetTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CelsiusRB
+            // 
+            this.CelsiusRB.AutoSize = true;
+            this.CelsiusRB.Location = new System.Drawing.Point(212, 65);
+            this.CelsiusRB.Name = "CelsiusRB";
+            this.CelsiusRB.Size = new System.Drawing.Size(62, 19);
+            this.CelsiusRB.TabIndex = 0;
+            this.CelsiusRB.TabStop = true;
+            this.CelsiusRB.Text = "Celsius";
+            this.CelsiusRB.UseVisualStyleBackColor = true;
+            // 
+            // FahrenheitRB
+            // 
+            this.FahrenheitRB.AutoSize = true;
+            this.FahrenheitRB.Location = new System.Drawing.Point(212, 90);
+            this.FahrenheitRB.Name = "FahrenheitRB";
+            this.FahrenheitRB.Size = new System.Drawing.Size(81, 19);
+            this.FahrenheitRB.TabIndex = 1;
+            this.FahrenheitRB.TabStop = true;
+            this.FahrenheitRB.Text = "Fahrenheit";
+            this.FahrenheitRB.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Anna asteet:";
+            // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.Location = new System.Drawing.Point(312, 75);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(71, 25);
+            this.MuunnaBT.TabIndex = 3;
+            this.MuunnaBT.Text = "button1";
+            this.MuunnaBT.UseVisualStyleBackColor = true;
+            this.MuunnaBT.Click += new System.EventHandler(this.MuunnaBT_Click);
+            // 
+            // AsteetTB
+            // 
+            this.AsteetTB.Location = new System.Drawing.Point(153, 77);
+            this.AsteetTB.Name = "AsteetTB";
+            this.AsteetTB.Size = new System.Drawing.Size(53, 23);
+            this.AsteetTB.TabIndex = 4;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(75, 126);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(46, 15);
+            this.VastausLB.TabIndex = 5;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 556);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.AsteetTB);
+            this.Controls.Add(this.MuunnaBT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FahrenheitRB);
+            this.Controls.Add(this.CelsiusRB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton CelsiusRB;
+        private System.Windows.Forms.RadioButton FahrenheitRB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MuunnaBT;
+        private System.Windows.Forms.TextBox AsteetTB;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
