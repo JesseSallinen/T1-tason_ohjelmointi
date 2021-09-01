8b8b5df046834d79b84808da9d4dae867abae2c2
JesseSallinen <jesse.sallinen03@gmail.com>
2021-09-01 16:01:51 +0300

    Noppa tehtävä

---
+++ Harjoitus_4/Harjoitus 4/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Harjoitus_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SyntymaAikaDT = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.VuosinaLB = new System.Windows.Forms.Label();
+            this.KuukausinaLB = new System.Windows.Forms.Label();
+            this.PaivinaLB = new System.Windows.Forms.Label();
+            this.TunteinaLB = new System.Windows.Forms.Label();
+            this.MinuutteinaLB = new System.Windows.Forms.Label();
+            this.SekunteinaLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SyntymaAikaDT
+            // 
+            this.SyntymaAikaDT.CalendarFont = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SyntymaAikaDT.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SyntymaAikaDT.Location = new System.Drawing.Point(12, 12);
+            this.SyntymaAikaDT.Name = "SyntymaAikaDT";
+            this.SyntymaAikaDT.Size = new System.Drawing.Size(331, 33);
+            this.SyntymaAikaDT.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(360, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Laske";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // VuosinaLB
+            // 
+            this.VuosinaLB.AutoSize = true;
+            this.VuosinaLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VuosinaLB.Location = new System.Drawing.Point(12, 70);
+            this.VuosinaLB.Name = "VuosinaLB";
+            this.VuosinaLB.Size = new System.Drawing.Size(23, 25);
+            this.VuosinaLB.TabIndex = 2;
+            this.VuosinaLB.Text = "X";
+            this.VuosinaLB.Visible = false;
+            // 
+            // KuukausinaLB
+            // 
+            this.KuukausinaLB.AutoSize = true;
+            this.KuukausinaLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.KuukausinaLB.Location = new System.Drawing.Point(12, 109);
+            this.KuukausinaLB.Name = "KuukausinaLB";
+            this.KuukausinaLB.Size = new System.Drawing.Size(23, 25);
+            this.KuukausinaLB.TabIndex = 3;
+            this.KuukausinaLB.Text = "X";
+            this.KuukausinaLB.Visible = false;
+            // 
+            // PaivinaLB
+            // 
+            this.PaivinaLB.AutoSize = true;
+            this.PaivinaLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PaivinaLB.Location = new System.Drawing.Point(12, 143);
+            this.PaivinaLB.Name = "PaivinaLB";
+            this.PaivinaLB.Size = new System.Drawing.Size(23, 25);
+            this.PaivinaLB.TabIndex = 4;
+            this.PaivinaLB.Text = "X";
+            this.PaivinaLB.Visible = false;
+            // 
+            // TunteinaLB
+            // 
+            this.TunteinaLB.AutoSize = true;
+            this.TunteinaLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TunteinaLB.Location = new System.Drawing.Point(209, 70);
+            this.TunteinaLB.Name = "TunteinaLB";
+            this.TunteinaLB.Size = new System.Drawing.Size(23, 25);
+            this.TunteinaLB.TabIndex = 5;
+            this.TunteinaLB.Text = "X";
+            this.TunteinaLB.Visible = false;
+            // 
+            // MinuutteinaLB
+            // 
+            this.MinuutteinaLB.AutoSize = true;
+            this.MinuutteinaLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MinuutteinaLB.Location = new System.Drawing.Point(209, 109);
+            this.MinuutteinaLB.Name = "MinuutteinaLB";
+            this.MinuutteinaLB.Size = new System.Drawing.Size(23, 25);
+            this.MinuutteinaLB.TabIndex = 6;
+            this.MinuutteinaLB.Text = "X";
+            this.MinuutteinaLB.Visible = false;
+            this.MinuutteinaLB.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // SekunteinaLB
+            // 
+            this.SekunteinaLB.AutoSize = true;
+            this.SekunteinaLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SekunteinaLB.Location = new System.Drawing.Point(209, 143);
+            this.SekunteinaLB.Name = "SekunteinaLB";
+            this.SekunteinaLB.Size = new System.Drawing.Size(23, 25);
+            this.SekunteinaLB.TabIndex = 7;
+            this.SekunteinaLB.Text = "X";
+            this.SekunteinaLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 578);
+            this.Controls.Add(this.SekunteinaLB);
+            this.Controls.Add(this.MinuutteinaLB);
+            this.Controls.Add(this.TunteinaLB);
+            this.Controls.Add(this.PaivinaLB);
+            this.Controls.Add(this.KuukausinaLB);
+            this.Controls.Add(this.VuosinaLB);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SyntymaAikaDT);
+            this.Name = "Form1";
+            this.Text = "Ikälaskuri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker SyntymaAikaDT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label VuosinaLB;
+        private System.Windows.Forms.Label KuukausinaLB;
+        private System.Windows.Forms.Label PaivinaLB;
+        private System.Windows.Forms.Label TunteinaLB;
+        private System.Windows.Forms.Label MinuutteinaLB;
+        private System.Windows.Forms.Label SekunteinaLB;
+    }
+}
+
