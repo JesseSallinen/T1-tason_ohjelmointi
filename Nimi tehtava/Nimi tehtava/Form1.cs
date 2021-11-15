60810a824e80837ed3a34954ad603c3b8366f28e
JesseSallinen <jesse.sallinen03@gmail.com>
2021-11-15 12:08:06 +0200

    moi

---
+++ Nimi tehtava/Nimi tehtava/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Nimi_tehtava
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TarkastaBT_Click(object sender, EventArgs e)
+        {
+            VastausLB.Text = "";
+            VastausLB.Visible = false;
+            string[] pojat = File.ReadAllLines("C:\\Users\\Tarja\\source\\repos\\T1-tason_ohjelmointi\\Nimi tehtava\\Pojat.txt");
+            string[] tytot = File.ReadAllLines("C:\\Users\\Tarja\\source\\repos\\T1-tason_ohjelmointi\\Nimi tehtava\\Tytot.txt");
+            string nimi = NimiTB.Text;
+            int laskurip = 1, laskurit = 1;
+            foreach(string poju in pojat)
+            {
+                if(nimi == poju)
+                {
+                    VastausLB.Text = "Nimesi on " + laskurip + ". suosituin poikien nimi vuonna 2020";
+                    VastausLB.Visible = true;
+                }
+                laskurip++;
+            }
+            foreach (string friidu in tytot)
+            {
+                if (nimi == friidu)
+                {
+                    VastausLB.Text = "Nimesi on " + laskurit + ". suosituin tyttöjen nimi vuonna 2020";
+                    VastausLB.Visible = true;
+                }
+                laskurit++;
+            }
+            if(VastausLB.Visible == false)
+            {
+                VastausLB.Text = "Nimesi ei löytynyt suosituimpien nimien joukosta! :-(";
+                VastausLB.Visible = true;
+            }
+        }
+    }
+}
---
+++ Sekuntikello/Sekuntikello/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Sekuntikello
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch Sekuntikello;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sekuntikello.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", Sekuntikello.Elapsed);
+        }
+
+        private void StopBT_Click(object sender, EventArgs e)
+        {
+            Sekuntikello.Stop();
+        }
+
+        private void ResetBT_Click(object sender, EventArgs e)
+        {
+            Sekuntikello.Reset();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Sekuntikello = new Stopwatch();
+        }
+    }
+}
